a5d797818952ff022d45194b9d0f696608a6d4b4
ehevutov <jamesgosnell@gmail.com>
2014-03-28 19:15:24 -0500

    ADDED initial class

---
+++ csharp-lib-exception-snmp/ExceptionWrap.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lib_exception_snmp
+{
+    public class Verification
+    {
+        static public virtual void verify<t>(ref t temp)
+        {
+            var props = typeof(ExceptionWrapConfig).GetProperties();
+            foreach (var prop in props)
+            {
+                if (prop == null) throw new System.ArgumentException();
+            }
+        }
+    }
+    public class ExceptionWrapConfig : Verification
+    {
+        public IPHostEntry hostEntry;
+        public string passphrase;
+        public int protocolVersion;
+
+        public ExceptionWrapConfig(IPHostEntry he, string pp, int pr)
+        {
+            hostEntry = he;
+            passphrase = pp;
+            protocolVersion = pr;
+            // insert verification here
+        }
+    }
+    public class ExceptionWrap : ExceptionWrapConfig
+    {
+        public ExceptionWrap();
+        public ~ExceptionWrap();
+
+        public void queue(string msg)
+        {
+
+        }
+    }
+}
