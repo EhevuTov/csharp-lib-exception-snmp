d0db72d08d62b1764ee0fe45c085930e6846e877
ehevutov <jamesgosnell@gmail.com>
2014-03-31 18:55:31 -0500

    ADDED exception config test

---
+++ csharp-lib-exception-snmpTests/ExceptionWrapConfigTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using csharp_lib_exception_snmp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace csharp_lib_exception_snmp.Tests
+{
+    [TestClass()]
+    public class ExceptionWrapConfigTests
+    {
+        [TestMethod()]
+        public void ExceptionWrapConfigTestConstructor1()
+        {
+            // arrange
+            // act
+            ExceptionWrapConfig config = new ExceptionWrapConfig();
+            // assert
+            Assert.AreEqual(config.passphrase, "public");
+        }
+
+        [TestMethod()]
+        public void ExceptionWrapConfigTestConstructor2()
+        {
+            Assert.Fail();
+        }
+    }
+}
