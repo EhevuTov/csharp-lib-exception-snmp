2ede8f404ab380253d5e7033848055f47afc6d35
James Gosnell <jamesgosnell@gmail.com>
2014-03-31 13:28:59 -0500

    ADDED test that is currently FAILING

---
+++ csharp-lib-exception-snmpTests/VerificationTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using csharp_lib_exception_snmp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace csharp_lib_exception_snmp.Tests
+{
+    // arrange
+    public class TestVerify
+    {
+        public int property1;
+        public double property2;
+        public string property3;
+    }
+    [TestClass()]
+    public class VerificationTests
+    {
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException),
+         "A property of null was inappropriately allowed.")]
+        public void verifyTest()
+        {
+            // arrange
+
+            // act
+            TestVerify testClass = new TestVerify();
+            Verification v       = new Verification();
+            v.verify(testClass);
+            // assert
+
+        }
+    }
+}
