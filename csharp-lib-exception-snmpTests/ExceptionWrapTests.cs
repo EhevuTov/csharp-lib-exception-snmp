e30e99e026b50db4eb6522eb0020417036ff2aef
James Gosnell <jamesgosnell@gmail.com>
2014-04-01 19:22:58 -0500

    ADDED exceptionWrap test

---
+++ csharp-lib-exception-snmpTests/ExceptionWrapTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using csharp_lib_exception_snmp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace csharp_lib_exception_snmp.Tests
+{
+    [TestClass()]
+    public class ExceptionWrapTests
+    {
+        [TestMethod()]
+        public void ExceptionWrapTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void sendTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
